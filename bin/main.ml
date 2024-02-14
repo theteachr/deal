e2835c2a72178436ff9e1740752e313aecf85a2e
theteachr <sop.theteachr@gmail.com>
2024-02-15 02:02:40 +0530

    Add a simple selector for cards in hand

--- bin/main.ml
+++ bin/main.ml
@@ -6,32 +6,29 @@ let init _ = Command.Noop
 let update event game =
   match event with
   | Event.KeyDown Escape -> (game, Command.Quit)
-  | Event.KeyDown (Key "p") -> (Game.end_turn game, Command.Noop)
+  | Event.KeyDown Enter -> (Game.end_turn game, Command.Noop)
+  | Event.KeyDown (Key "j") -> (Game.choose_next game, Command.Noop)
+  | Event.KeyDown (Key "k") -> (Game.choose_prev game, Command.Noop)
   | _ -> (game, Command.Noop)
 
-let view Game.{ table = player, _; deck; turn } =
-  Format.sprintf
-    {|
+let view Game.{ table = player, _; deck; turn; state } =
+  let view_state = function
+    | Game.Choosing_from_hand selected ->
+        player.hand
+        |> List.mapi (fun i card ->
+               let bullet = if i = selected then ">" else " " in
+               Printf.sprintf "%s %s" bullet (Card.show card))
+        |> String.concat "\n"
+  in
+  Format.sprintf {|
 
 %s is playing (%d).
 
--- hand --
-
 %s
 
-┌───────────┐
-│           │
-│           │
-│           │
-│   %3d     │
-│           │
-│           │
-│ Deck      │
-└───────────┘
+%d card(s) in the deck.
   |}
-    player.name turn
-    (Player.Hand.to_string player.hand)
-    (Deck.count deck)
+    player.name turn (view_state state) (Deck.count deck)
 
 let deal = Minttea.app ~init ~update ~view ()
 
--- lib/game.ml
+++ lib/game.ml
@@ -7,18 +7,38 @@ module Table = struct
     | _ -> failwith "should not be empty"
 end
 
+type state = Choosing_from_hand of int
+
 type t = {
   table : Table.t;
   deck : Deck.t;
   turn : int;
+  state : state;
 }
 
-let start_turn { table = player, opponents; deck; turn } =
+let choose_prev ({ table = player, _; state; _ } as game) =
+  let state =
+    match state with
+    | Choosing_from_hand i ->
+        let length = List.length player.hand in
+        Choosing_from_hand ((i + length - 1) mod length)
+  in
+  { game with state }
+
+let choose_next ({ table = player, _; state; _ } as game) =
+  let state =
+    match state with
+    | Choosing_from_hand i ->
+        Choosing_from_hand ((i + 1) mod List.length player.hand)
+  in
+  { game with state }
+
+let start_turn { table = player, opponents; deck; turn; _ } =
   let n = if Player.empty_hand player then 5 else 2 in
   let cards, deck = Deck.draw n deck in
   let player = Player.update_hand player cards in
   let table = (player, opponents) in
-  { table; deck; turn = turn + 1 }
+  { table; deck; turn = turn + 1; state = Choosing_from_hand 0 }
 
 let end_turn ({ table; _ } as game : t) : t =
   { game with table = Table.turn table } |> start_turn
@@ -31,4 +51,4 @@ let create players =
     | current :: opponents -> (current, opponents)
     | [] -> failwith "no players"
   in
-  { table; deck = Deck.default; turn = 0 }
+  { table; deck = Deck.default; turn = 0; state = Choosing_from_hand 0 }
